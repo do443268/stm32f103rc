76a025fb34c88085a050d517cb063901dafbeae7
do443268 <do443268@gmail.com>
2021-06-25 16:26:36 +0700

    add

--- keil/GPIO/main.c
+++ keil/GPIO/main.c
@@ -7,66 +7,74 @@ int count = 0;
 int main(void)
 {
 	// goi chuong trinh con da khai bao
-	GPIO_Configuration();                                 
-  while (1)
-  {
+	GPIO_Configuration();
+	while (1)
+	{
 
-			Button();
-//			if(count == 1){
-//				GPIO_WriteBit(GPIOD,GPIO_Pin_2,(BitAction)Bit_SET);
-//			}
-//			if(count == 2){
-//				GPIO_WriteBit(GPIOD,GPIO_Pin_2,(BitAction)Bit_RESET);
-//			}
-//		GPIO_WriteBit(GPIOD,GPIO_Pin_2,(BitAction)(1^GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1)));
-			//button();
-  }
+		Button();
+		// if (count == 1)
+		// {
+		// 	GPIO_WriteBit(GPIOD, GPIO_Pin_2, (BitAction)Bit_SET);
+		// }
+		// if (count == 2)
+		// {
+		// 	GPIO_WriteBit(GPIOD, GPIO_Pin_2, (BitAction)Bit_RESET);
+		// }
+		// GPIO_WriteBit(GPIOD, GPIO_Pin_2, (BitAction)(1 ^ GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1)));
+	}
 }
 
 void GPIO_Configuration(void)
 {
-	GPIO_InitTypeDef 	GPIO_InitStructure;
-	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC|RCC_APB2Periph_GPIOD, ENABLE); // cap clock cho Port D, Port C
-	// cau hinh chan I/O su dung la PD2,toc do input,mode input la uotput pull-up	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;	
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;			
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;		
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD, ENABLE); // cap clock cho Port D, Port C
+	// cau hinh chan I/O su dung la PD2,toc do input,mode input la uotput pull-up
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
-	GPIO_ResetBits(GPIOD,GPIO_Pin_2);
-	// cau hinh chan I/O su dung la PC1,toc do input,mode input la input pull-up	
+	GPIO_ResetBits(GPIOD, GPIO_Pin_2);
+	// cau hinh chan I/O su dung la PC1,toc do input,mode input la input pull-up
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;			
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;		
-	GPIO_Init(GPIOC, &GPIO_InitStructure);	
-	
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 // ham delay tuong doi dung vong lap while
-void Delay_ms(uint16_t time)														
+void Delay_ms(uint16_t time)
 {
 	// tang bien dem len 12000 lan
-	uint32_t time_n=time*12000;		
-	// cho den khi biem time_n giam =0 thi thoat	
-	while(time_n!=0){time_n--;}													
-
+	uint32_t time_n = time * 12000;
+	// cho den khi biem time_n giam =0 thi thoat
+	while (time_n != 0)
+	{
+		time_n--;
+	}
 }
- void Button(void){
- 			if(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == 0){
-				Delay_ms(10);
-				if(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == 0){
-				while(GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == 0){	
-					}
-				count++;
-				if(count > 2){
-					count = 1;
-						}
-				}
+void Button(void)
+{
+	if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == 0)
+	{
+		Delay_ms(10);
+		if (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == 0)
+		{
+			while (GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1) == 0)
+			{
 			}
-			if(count == 1){
-					GPIO_WriteBit(GPIOD,GPIO_Pin_2,(BitAction)Bit_SET);
+			count++;
+			if (count > 2)
+			{
+				count = 1;
 			}
-			if(count == 2){
-					GPIO_WriteBit(GPIOD,GPIO_Pin_2,(BitAction)Bit_RESET);
-			}
- }
-
+		}
+	}
+	if (count == 1)
+	{
+		GPIO_WriteBit(GPIOD, GPIO_Pin_2, (BitAction)Bit_SET);
+	}
+	if (count == 2)
+	{
+		GPIO_WriteBit(GPIOD, GPIO_Pin_2, (BitAction)Bit_RESET);
+	}
+}
---
+++ keil/TIME/main.c
@@ -0,0 +1,84 @@
+#include "stm32f10x.h"
+
+void GPIO_Configuration(void);
+void TIM4_config(void);
+void blink(void);
+void delay_ms_4(unsigned int time);
+
+int main(void)
+{
+	GPIO_Configuration();
+	TIM4_config();
+	while (1)
+	{
+		blink();
+	}
+}
+
+void delay_us_4(unsigned int time) {
+   while(time--)
+	 {
+     TIM4->CNT = 0;
+     while (TIM4->CNT < 1) {}
+	 }
+}
+
+void delay_ms_4(unsigned int time) {
+    while (time--) {
+        delay_us_4(1000);
+    }
+}
+
+void delay_ms(uint16_t time)
+{
+	// tang bien dem len 12000 lan
+	uint32_t time_n = time * 12000;
+	// cho den khi biem time_n giam =0 thi thoat
+	while (time_n != 0)
+	{
+		time_n--;
+	}
+}
+
+void blink(void)
+{
+		GPIO_SetBits(GPIOD, GPIO_Pin_2);
+		delay_ms_4(1000);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_2);
+		delay_ms_4(1000);
+}
+
+void GPIO_Configuration(void) // gpio config
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOD, ENABLE); // cap clock cho Port D, Port A
+	// cau hinh chan I/O su dung la PD2,toc do input,mode input la uotput pull-up
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_ResetBits(GPIOD, GPIO_Pin_2);
+	// cau hinh chan I/O su dung la PC1,toc do input,mode input la input pull-up
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+void TIM4_config(void) // timer 4 config
+{
+	TIM_TimeBaseInitTypeDef TimerInit;
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	
+	TimerInit.TIM_CounterMode = TIM_CounterMode_Up; // mode dem len
+	TimerInit.TIM_Period = 1; // bo chia
+	TimerInit.TIM_Prescaler = SystemCoreClock / 1000000 - 1;//  gia tri dem nap vao timer (0x0000 -> 0xffff)
+	
+	TimerInit.TIM_ClockDivision = TIM_CKD_DIV1; // chia clock
+  TimerInit.TIM_RepetitionCounter = 0; // gia tri bo dem lap lai
+	TIM_TimeBaseInit(TIM4, &TimerInit); // init cac tham so bo dem
+	
+	TIM_Cmd(TIM4,ENABLE); // chay bo dem
+}
+
